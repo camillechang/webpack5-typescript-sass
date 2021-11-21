3f53359708e89ddb3b09c116d6d134bec29fbf73
Camille Chang <camille7710@yahoo.com>
2021-11-21 18:09:16 +1100

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,50 @@
-const path = require('path');
-//load all .ts and .tsx files through the ts-loader, and output a bundle.js file in our current directory.
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map', //tell webpack to extract these source maps and include in our final bundle:
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    index: './src/index.tsx',
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+  };
+}
+
+module.exports = config;
