1d78593c18cabbcfb123d2c39610d5362d4bdeb0
Camille Chang <camille7710@yahoo.com>
2021-11-21 14:57:00 +1100

    add ts support

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
-
+//load all .ts and .tsx files through the ts-loader, and output a bundle.js file in our current directory.
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map', //tell webpack to extract these source maps and include in our final bundle:
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
